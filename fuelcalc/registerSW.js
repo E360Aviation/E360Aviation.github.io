74a13d0da41bd74b3dd5b98a32489cf1288eff62
Travis Lesicka <59949694+tlesicka@users.noreply.github.com>
2025-09-13 21:13:41 +1000

    Update fuelcalc from hardcoded location to relative location

--- fuelcalc/registerSW.js
+++ fuelcalc/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/fuelcalc/sw.js', { scope: '/fuelcalc/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
